46e393029355c325b48a5060f137cf21b83818f4
rattphumi <darkdamonig@gmail.com>
2024-11-29 16:16:45 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+    int even_num = 0;
+    int odd_num = 0;
+    int num = 0;
+
+    do
+    {
+        cout << "Enter an integer: ";
+        cin >> num;
+
+        if (num%2 == 0 && num != 0)
+        {
+            even_num++;
+        }
+        else if(num%2 == 1){
+
+            odd_num++;
+        }
+        
+        
+    } while (num != 0);
+    
+    
+    cout << "#Even numbers = " << even_num << endl;
+    cout << "#Odd numbers = " << odd_num << endl;
     return 0;
 }
